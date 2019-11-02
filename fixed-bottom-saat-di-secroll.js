1791b4c8d02cd37aa8fe3ba11b2868b0b75b2494
kirmanalidris <56144888+kirmanalidris@users.noreply.github.com>
2019-11-02 19:36:30 +0700

    Update fixed-bottom-saat-di-secroll.js

--- fixed-bottom-saat-di-secroll.js
+++ fixed-bottom-saat-di-secroll.js
@@ -2,10 +2,12 @@ var nav = document.getElementById('kirman-scrool-fixed-bottom');
 
        window.onscroll = function () {
 
-         if(window.pageYOffset > 100){
+         if(window.pageYOffset > 400){
 
           nav.style.position = "fixed";
           nav.style.bottom = 0;
+          nav.style.right = 0;
+          nav.style.left = 0;
 
           }else{
             // nav.style.position = "absolute";
